62d2ab880b485b81f54e9f66c87b95cb404d7f72
sazzad <sazadssd612@gmail.com>
2024-05-12 20:55:30 +0600

    image_path_add

--- src/Pages/Portfolio/Portfolio.jsx
+++ src/Pages/Portfolio/Portfolio.jsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 import project1 from "../../assets/project1.png";
 import project2 from "../../assets/project3.png";
 import project3 from "../../assets/project4.png";
-import project5 from "../../assets/project5";
+import project5 from "../../assets/project5.png";
 
 const projectList = [
   {
